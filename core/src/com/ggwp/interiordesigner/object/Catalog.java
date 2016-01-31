506b04aae9562b9054585e3e6ecc7028c057c426
Raymond Francisco <raymond051992@gmail.com>
2016-02-01 01:19:32 +0800

    added models

--- core/src/com/ggwp/interiordesigner/object/Catalog.java
+++ core/src/com/ggwp/interiordesigner/object/Catalog.java
@@ -292,7 +292,6 @@ public class Catalog extends Window {
         categories.addActor(createCategoryContainer("Book Shelves", "furnitures/categories/book.png", defaultTextButtonStyle, bookShelfClickListener));
         categories.addActor(createCategoryContainer("Mirrors", "furnitures/categories/mirror.png", defaultTextButtonStyle, mirrorClickListener));
         categories.addActor(createCategoryContainer("Dining Set", "furnitures/categories/dining.png", defaultTextButtonStyle, diningClickListener));
-        categories.addActor(createCategoryContainer("Kitchen Cabinets", "furnitures/categories/kitchen.png", defaultTextButtonStyle, kitchenClickListener));
         categories.addActor(createCategoryContainer("Wall Clock", "furnitures/categories/wallclock.png", defaultTextButtonStyle, wallClockClickListener));
         categories.addActor(createCategoryContainer("TV", "furnitures/categories/tv.png", defaultTextButtonStyle, tvClickListener));
         categories.addActor(createCategoryContainer("Washing Machine", "furnitures/categories/washingmachine.png", defaultTextButtonStyle, washingMachineClickListener));
@@ -502,13 +501,10 @@ public class Catalog extends Window {
         main.add(new Label("Wall Clocks", SkinManager.getDefaultLabelStyle())).colspan(3);
         main.row();
 
-        for(int i = 1; i <= 9; i++){
-            main.add(createFurnitureCard("Wall Clock " + i, "furnitures/wallclock/" + i + ".png", null));
 
-            if(i % 3 == 0){
-                main.row();
-            }
-        }
+        main.add(createFurnitureCard("Grand Hotel", "furnitures/wallclock/1/1.jpg", createListener("furnitures/wallclock/1/1.obj", GameObject.TYPE_WALL_OBJECT)));
+        main.add(createFurnitureCard("Plain", "furnitures/wallclock/2/2.jpg", createListener("furnitures/wallclock/2/2.obj", GameObject.TYPE_WALL_OBJECT)));
+
         return new Container(main);
     }
 
@@ -581,6 +577,7 @@ public class Catalog extends Window {
         main.row();
         main.add(createFurnitureCard("Cafelle Wood", "furnitures/cabinets/4/4.jpg", createListener("furnitures/cabinets/4/4.obj", GameObject.TYPE_FLOOR_OBJECT)));
         main.add(createFurnitureCard("Corner Dresser", "furnitures/cabinets/5/5.jpg", createListener("furnitures/cabinets/5/5.obj", GameObject.TYPE_FLOOR_OBJECT)));
+        main.add(createFurnitureCard("Base Open", "furnitures/cabinets/6/6.jpg", createListener("furnitures/cabinets/6/6.obj", GameObject.TYPE_WALL_OBJECT)));
         return new Container(main);
     }
 
